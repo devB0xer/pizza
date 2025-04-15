119819a98009f8ead02e44e60634b91570deac5c
devB0xer <bazuev.4@yandex.ru>
2025-04-15 10:52:31 +0400

    pages, entity, admin-dashboard

--- src/Controller/MenuPageController.php
+++ src/Controller/MenuPageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PizzaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MenuPageController extends AbstractController
 {
     #[Route('/menu', name: 'app_menu')]
-    public function index(): Response
+    public function index(PizzaRepository $pizzaRepository): Response
     {
+        $pizzas = $pizzaRepository->findAll();
+
         return $this->render('menu_page/index.html.twig', [
-            'controller_name' => 'MenuPageController',
+            'pizzas' => $pizzas,
         ]);
     }
 }
